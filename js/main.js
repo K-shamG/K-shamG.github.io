adf48c0b4d1adaf76032434abefead30f4fef009
Kshamina <kshamina.ghelani@carleton.ca>
2019-06-26 23:39:06 -0400

    added resume moda, sidebar, parallax

---
+++ js/main.js
@@ -0,0 +1,5 @@
+$(document).on("click", "#hide-navbar, #show-navbar", function() {
+    $("#navbarsExample04").toggleClass("d-none");
+    $("#hide-navbar").toggleClass("d-none");
+    $("#show-navbar").toggleClass("d-none");
+})
