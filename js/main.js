7ba463d1404a6cdcd28e4f57497f9639458a512e
Kshamina <kshamina.ghelani@carleton.ca>
2019-07-04 11:46:49 -0400

    responsive stufff

--- js/main.js
+++ js/main.js
@@ -3,3 +3,13 @@ $(document).on("click", "#hide-navbar, #show-navbar", function() {
     $("#hide-navbar").toggleClass("d-none");
     $("#show-navbar").toggleClass("d-none");
 })
+
+function toggleSideNav() {
+    if($(".sidenav").css("width") == "0px") {
+        $(".sidenav").css({"width": "100%", "height": "auto", "margin-top": "50px"});
+    }
+    else {
+        $(".sidenav").css({"width": "0", "margin-top": "0px" });
+    }
+
+}
