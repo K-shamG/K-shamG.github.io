6e6904cb4256ae9ea260471993d6205545c3a16b
Kshamina <kshamina.ghelani@carleton.ca>
2019-07-04 12:19:49 -0400

    sidenav responsive

--- js/main.js
+++ js/main.js
@@ -6,10 +6,10 @@ $(document).on("click", "#hide-navbar, #show-navbar", function() {
 
 function toggleSideNav() {
     if($(".sidenav").css("width") == "0px") {
-        $(".sidenav").css({"width": "100%", "height": "auto", "margin-top": "50px"});
+        $(".sidenav").css({"width": "100%", "height": "auto", "margin": "50px 5px 0px 5px"});
     }
     else {
-        $(".sidenav").css({"width": "0", "margin-top": "0px" });
+        $(".sidenav").css({"width": "0", "margin": "0px" });
     }
 
 }
